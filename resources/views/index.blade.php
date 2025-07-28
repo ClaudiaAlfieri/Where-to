bf7bb2129de4055305f85df2b2369d15fabe6ece
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2025-07-28 16:31:25 +0100

    Create layout

--- resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layout')
-
-<body>
-    
-</body>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Cookbook - Transforme seus ingredientes em receitas incríveis</title>
+    <title>WhereTo</title>
 
     <!-- PNG favicons -->
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon-16x16.png') }}">
@@ -35,11 +35,11 @@
 
 <body>
     <nav
-        class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        class=" nav dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-2 px-4">
             <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="{{ asset('assets/img/logo_sem_texto.png') }}" class="h-16" alt="Cookbook Logo">
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white pr-10">CookBook</span>
+                <img src="{{ asset('assets/img/logo_sem_texto.png') }}" class="h-16" alt="WhereTo Logo">
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white pr-10">Where To?</span>
             </a>
             <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                 <button data-collapse-toggle="navbar-sticky" type="button"
@@ -55,7 +55,7 @@
             </div>
             <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
                 <ul
-                    class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    class="flex flex-col p-4 md:p-0 mt-4 font-medium border md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                     <li>
                         <a href="/"
                             class="nav-link-mobile block py-2 px-3 text-white bg-yellow-700 rounded-sm md:bg-transparent md:text-yellow-700 md:p-0 md:dark:text-yellow-500"
@@ -72,7 +72,7 @@
                     </li>
                 </ul>
 
-                <!-- Adicione a classe mobile-cta-container aqui -->
+                <!-- Cta -->
                 <div class="ml-10 md:ml-10 mobile-cta-container">
                     <a href="#receita" class="nav-link-mobile">
                         <button type="button" class="cta-nav-btn w-full md:w-auto">
