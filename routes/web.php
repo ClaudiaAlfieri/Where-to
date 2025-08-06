17cbcce821d7881b82b6bbfb875aede3801c3d62
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2025-08-06 22:11:39 +0100

    Routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Rota para gerar roteiro:
+Route::post('/generate-travel', [TravelController::class, 'generateTravel']);
